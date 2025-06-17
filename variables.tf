c63fbc3094a5d68cdf3937a866c4b49ee81d5f57
nag9844 <96878041+nag9844@users.noreply.github.com>
2025-06-17 14:14:34 +0530

    initial commit

--- variables.tf
+++ variables.tf
@@ -20,7 +20,7 @@ variable "ami_id" {
   description = "AMI ID for EC2 instances"
   type        = string
   default     = "ami-0f918f7e67a3323f0" 
-
+}
 
 variable "security_group" {
   description = "security group id"
