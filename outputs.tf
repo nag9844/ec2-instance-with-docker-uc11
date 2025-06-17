ec8f91deafca53e01e87acdf24179c2ed7809ea6
nag9844 <96878041+nag9844@users.noreply.github.com>
2025-06-17 14:24:59 +0530

    public ip

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,8 @@ output "instance_id" {
   description = "The ID of the web_server EC2 instance"
   value       = aws_instance.web_server.id
 }
+
+output "public_ip" {
+  description = "The public IP of the web_server EC2 instance"
+  value       = aws_instance.web_server.public_ip
+}
